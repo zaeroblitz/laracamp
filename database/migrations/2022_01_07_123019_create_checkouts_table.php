d4d7015c84766eee1083c8f1afece5837c23e001
Zaero Blitz <zaeroblitz@gmail.com>
2022-01-07 19:33:58 +0700

    finish migration

--- database/migrations/2022_01_07_123019_create_checkouts_table.php
+++ database/migrations/2022_01_07_123019_create_checkouts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCampBenefitsTable extends Migration
+class CreateCheckoutsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,16 @@ class CreateCampBenefitsTable extends Migration
      */
     public function up()
     {
-        Schema::create('camp_benefits', function (Blueprint $table) {
+        Schema::create('checkouts', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained();
             $table->foreignId('camp_id')->constrained();
-            $table->string('name');
+            $table->string('card_number', 20);
+            $table->date('expired');
+            $table->string('cvc', 3);
+            $table->boolean('is_paid')->default(false);
             $table->timestamps();
-
+            $table->softDeletes();
         });
     }
 
@@ -29,6 +33,6 @@ class CreateCampBenefitsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('camp_benefits');
+        Schema::dropIfExists('checkouts');
     }
 }
