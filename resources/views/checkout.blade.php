aa12329729fd705c875fa17b9d594821a06c3733
Zaero Blitz <zaeroblitz@gmail.com>
2022-01-08 18:55:16 +0700

    Slicing Checkout Page

--- resources/views/checkout.blade.php
+++ resources/views/checkout.blade.php
@@ -31,7 +31,7 @@
                     <div class="col-lg-1 col-12"></div>
                     <div class="col-lg-6 col-12">
                         @csrf
-                        <form action="#" class="basic-form">
+                        <form action="{{ route('success-checkout') }}" class="basic-form">
                             <div class="mb-4">
                                 <label for="exampleInputEmail1" class="form-label">Full Name</label>
                                 <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
